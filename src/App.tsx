749ab17c5dcd6f24985d771517856e5583daa07f
Demon_Daddyyyy <biancasemedo@gmail.com>
2026-08-13 23:46:27 +0100

    mais um concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
-
 import { GlobalStyle } from './styles'
+import { store, AppDispatch } from './containers/store'
+import { Provider, useDispatch } from 'react-redux'
+import { adicionar } from './containers/store/reducers/carrinho'
+import { favoritar } from './containers/store/reducers/favoritos'
 
 export type Produto = {
   id: number
@@ -13,8 +16,7 @@ export type Produto = {
 
 function App() {
   const [produtos, setProdutos] = useState<Produto[]>([])
-  const [carrinho, setCarrinho] = useState<Produto[]>([])
-  const [favoritos, setFavoritos] = useState<Produto[]>([])
+  const dispatch = useDispatch<AppDispatch>()
 
   useEffect(() => {
     fetch('https://api-ebac.vercel.app/api/ebac_sports')
@@ -23,35 +25,25 @@ function App() {
   }, [])
 
   function adicionarAoCarrinho(produto: Produto) {
-    if (carrinho.find((p) => p.id === produto.id)) {
-      alert('Item já adicionado')
-    } else {
-      setCarrinho([...carrinho, produto])
-    }
+    dispatch(adicionar(produto))
   }
 
-  function favoritar(produto: Produto) {
-    if (favoritos.find((p) => p.id === produto.id)) {
-      const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id)
-      setFavoritos(favoritosSemProduto)
-    } else {
-      setFavoritos([...favoritos, produto])
-    }
+  function favoritarProduto(produto: Produto) {
+    dispatch(favoritar(produto))
   }
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header favoritos={favoritos} itensNoCarrinho={carrinho} />
+        <Header />
         <Produtos
           produtos={produtos}
-          favoritos={favoritos}
-          favoritar={favoritar}
+          favoritar={favoritarProduto}
           adicionarAoCarrinho={adicionarAoCarrinho}
         />
       </div>
-    </>
+    </Provider>
   )
 }
 
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,26 +1,28 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useSelector } from 'react-redux'
+import { RootReducer } from './store'
 
 import * as S from './styles'
 
 type Props = {
   produtos: ProdutoType[]
-  favoritos: ProdutoType[]
   adicionarAoCarrinho: (produto: ProdutoType) => void
   favoritar: (produto: ProdutoType) => void
 }
 
 const ProdutosComponent = ({
   produtos,
-  favoritos,
   adicionarAoCarrinho,
   favoritar
 }: Props) => {
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    const idsDosFavoritos = favoritos.map((f) => f.id)
 
-    return IdsDosFavoritos.includes(produtoId)
+    return idsDosFavoritos.includes(produtoId)
   }
 
   return (
@@ -31,13 +33,12 @@ const ProdutosComponent = ({
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
             aoComprar={adicionarAoCarrinho}
+            favoritar={favoritar}
           />
         ))}
       </S.Produtos>
     </>
   )
 }
-
 export default ProdutosComponent
